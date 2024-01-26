bff7c56e2f517aeb04a79e597df6571183f977d6
Hampus Eriksson <Hampus.Eriksson@elev.ga.ntig.se>
2024-01-26 10:18:25 +0100

    1st commit

---
+++ APIc#/Digimon.cs
@@ -0,0 +1,5 @@
+﻿public class Digimon
+{
+    public string name { get; set; }
+    public string releaseDate { get; set; }
+}
---
+++ APIc#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using RestSharp;
+using System.IO;
+
+//Grund url
+RestClient client = new("https://digi-api.com/api/v1/");
+
+while (true)
+{
+    Console.WriteLine("Which digimon?");
+    string digi = Console.ReadLine();
+    //Specificerad url förlängning
+    RestRequest request = new($"digimon/{digi}");
+
+    //Resultat
+    RestResponse response = client.GetAsync(request).Result;
+
+    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+    {
+        Digimon d = JsonSerializer.Deserialize<Digimon>(response.Content);
+
+        Console.WriteLine("name: " + d.name + ", release date: " + d.releaseDate);
+    }
+    else
+        Console.WriteLine(response.StatusCode);
+
+    Console.ReadLine();
+}
+// Console.WriteLine(response.Content);
+// File.WriteAllText("pokemon.json", response.Content);
+
